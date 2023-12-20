f4953429d578e4cdb113caa893f046dcaf409512
TouhouFanatic <144213577+TouhouFanatic@users.noreply.github.com>
2023-12-20 11:26:59 +0400

    Chapter 1

---
+++ Chapter 1- Getting Started with Python/Exercises/Ch1A4.py
@@ -0,0 +1,5 @@
+vara = "Hello,"
+varb = "I am"
+varc = "Vince"
+
+print (vara,varb,varc)
---
+++ Chapter 1- Getting Started with Python/Exercises/Ch1A5.py
@@ -0,0 +1,5 @@
+from math import pi
+circle = float(input("Input the radius of the circle : "))
+area = pi * circle ** 2
+
+print("Circle Radius: " + str(area))

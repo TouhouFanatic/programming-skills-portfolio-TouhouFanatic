120addfe2bbe72f8affb667b765e04d4a0edef50
TouhouFanatic <144213577+TouhouFanatic@users.noreply.github.com>
2024-01-17 11:58:32 +0400

    Chapter 4

--- Chapter 4- Control Flow/Exercises/Ch4A1.py
+++
@@ -1,7 +0,0 @@
-alien = 'green'
-if alien == 'green':
-    print('+5 XP')
-
-alien = 'red'
-if alien == 'green':
-    print('+5 XP')
--- Chapter 5- Dictionaries/Testing.py
+++
@@ -1,4 +0,0 @@
-dictionary = {'Name' : 'AAAAAAAAA' ,
-              'Roll No ' : '1234' ,
-               'Mothers Name' : 'EEEEEEE'}
-print(dictionary.items())

c695d3892d32d8648df97e656a25b019ad167652
TouhouFanatic <144213577+TouhouFanatic@users.noreply.github.com>
2024-01-17 12:01:05 +0400

    Chapter 4

---
+++ Chapter 4- Control Flow/Exercises/Ch4A1.py
@@ -0,0 +1,7 @@
+alien = 'green'
+if alien == 'green':
+    print('+5 XP')
+
+alien = 'red'
+if alien == 'green':
+    print('+5 XP')
---
+++ Chapter 4- Control Flow/Exercises/Ch4A2.py
@@ -0,0 +1,11 @@
+alien = 'green'
+if alien == 'green':
+    print('+5 XP')
+else:
+    print('+10 XP')
+
+alien = 'red'
+if alien == 'green':
+    print('+5 XP')
+else:
+    print('+10 XP')
